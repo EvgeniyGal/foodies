64a13ac06fe8582bc47ff7f56d6cbcbfe9ac88ba
Yuriy-St <yuriystaynov@gmail.com>
2024-06-05 00:13:29 +0300

    feat(user): update avatar

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,10 +3,13 @@ import express from 'express';
 import { loginUserSchema, registerUserSchema } from '../schemas/usersSchemas.js';
 import validateBody from '../decorators/validateBody.js';
 import usersController from '../controllers/usersController.js';
+import authenticate from '../middleware/authenticate.js';
+import { uploadAvatar } from '../middleware/upload.js';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/register', validateBody(registerUserSchema), usersController.register);
 usersRouter.post('/login', validateBody(loginUserSchema), usersController.login);
+usersRouter.patch('/avatar', authenticate, uploadAvatar, usersController.updateAvatar);
 
 export default usersRouter;
