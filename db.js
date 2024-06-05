f7b70a2bae28d89fe6ffef0f8aafe3df8ed1d99f
Yuriy-St <yuriystaynov@gmail.com>
2024-06-05 21:56:18 +0300

    fix(project): refactor db.js and app.js files

--- db.js
+++ db.js
@@ -1,4 +1,21 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
+import app from './app.js';
+
+const {PORT, NODE_ENV, TEST_DB_URL, DEV_DB_URL, PROD_DB_URL } = process.env;
+
+function getDbUrl() {
+  switch (NODE_ENV) {
+    case 'test':
+      return TEST_DB_URL;
+    case 'dev':
+      return DEV_DB_URL;
+    case 'prod':
+      return PROD_DB_URL;
+    default:
+      return DEV_DB_URL;
+  }
+}
 
 export const db = () =>
   mongoose
@@ -13,15 +30,13 @@ export const db = () =>
 
 export const closeDb = () => mongoose.connection.close();
 
-function getDbUrl() {
-  switch (process.env.NODE_ENV) {
-    case 'test':
-      return process.env.TEST_DB_URL;
-    case 'dev':
-      return process.env.DEV_DB_URL;
-    case 'prod':
-      return process.env.PROD_DB_URL;
-    default:
-      return process.env.DEV_DB_URL;
-  }
+
+if (NODE_ENV === 'prod' || NODE_ENV === 'dev') {
+  await db();
+
+  app.listen(PORT, () => {
+    console.log(
+      `App running. Use our API on port: ${PORT}`
+    );
+  });
 }
